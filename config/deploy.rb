78803a4520faa456d2f72c59b1797189f897868e
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-01 12:41:57 +0900

    deploy.rbの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :application, 'nominomo'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:tomokazu-sema/nominomo.git'
+set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
