0bd71c7e2c652970260b5fe60347c4941b2fe6b3
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-01 17:39:01 +0900

    uidの生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # ランダム文字列をuidカラムに格納
+  uid_num_char = 8
+  uid_ranges   = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+  generate_public_uid column: :uid, generator: PublicUid::Generators::RangeString.new(uid_num_char, uid_ranges)
+
+  # urlの:idをuidの値に置き換える
+  def to_param
+    uid
+  end
 end
