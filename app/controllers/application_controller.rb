f4a639f0e509a0a5a2c5a2b2bcd570253ba2897d
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-05 17:30:04 +0900

    ユーザーページの仮修正、編集ページの仮作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller?
 
   private
 
   # userモデルで:nameを受け取れるようにする
+  ### ログイン
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  ### 編集 
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
