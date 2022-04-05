ec5cd99dccd7b225d9890b60d3c68e8743f8c475
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-05 11:53:22 +0900

    uid生成の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # ランダム文字列をuidカラムに格納
-  uid_num_char = 8
+  uid_num_char = 16
   uid_ranges   = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
   generate_public_uid column: :uid, generator: PublicUid::Generators::RangeString.new(uid_num_char, uid_ranges)
 
