606e5e57d25feb685e28938515ffef4ecdbfde87
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-05 12:30:42 +0900

    テストコードの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it 'uid、name、email、password、password_confirmationが存在する場合は登録できる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できない場合' do
+      it 'uidが空では登録できない' do
+        @user.uid = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Uid can't be blank")
+      end
+      it 'nameが空では登録できない' do
+        @user.name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'emailが重複している場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'emailに@が含まれていない場合は登録できない' do
+        @user.email = 'testsample'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが5文字以下の場合は登録できない' do
+        @user.password = Faker::Lorem.characters(number: 5)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordがpassword_confirmationと一致していない場合は登録できない' do
+        @user.password_confirmation = Faker::Lorem.characters(number: 7)
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+  end
 end
