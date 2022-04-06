0fd7ce06df49773f5c8ecf82f656836dbed2f709
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-06 12:23:20 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_account_update_parameters, if: :devise_controller?
 
   private
+
+  # basic認証
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 
   # userモデルで:nameを受け取れるようにする
   ### ログイン
