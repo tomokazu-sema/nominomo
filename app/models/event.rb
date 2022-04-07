c6774f1daa91266dd9c9da01f8feb2588afc6b74
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-07 17:42:38 +0900

    eventモデルのバリデーション、uidの設定、変数名修正

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,16 @@
 class Event < ApplicationRecord
+  has_secure_password
+  
+  # ランダム文字列をuidカラムに格納
+  uid_length = 32
+  uid_ranges   = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
+  generate_public_uid column: :uid, generator: PublicUid::Generators::RangeString.new(uid_length, uid_ranges)
+
+  # urlの:idをuidの値に置き換える
+  def to_param
+    uid
+  end
+
+  # バリデーション
+  validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # ランダム文字列をuidカラムに格納
-  uid_num_char = 16
+  uid_length = 16
   uid_ranges   = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
-  generate_public_uid column: :uid, generator: PublicUid::Generators::RangeString.new(uid_num_char, uid_ranges)
+  generate_public_uid column: :uid, generator: PublicUid::Generators::RangeString.new(uid_length, uid_ranges)
 
   # urlの:idをuidの値に置き換える
   def to_param
