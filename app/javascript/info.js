362ed1d4c522dac56a230d9e57701048e63dcc68
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-12 14:44:15 +0900

    urlコピーボタンの修正

--- app/javascript/info.js
+++ app/javascript/info.js
@@ -1,15 +1,32 @@
+// 要改修 2022.04.12 t.sema
+// 重複部をfunctionにまとめるとwriteTextのpermissionを要求される
+// firefoxがPermission APIに対応していない
+
 function copyUrl () {
   const copyBtnGuest = document.getElementById("copy_btn_guest");
+  const copyBtnHost  = document.getElementById("copy_btn_host");
   copyBtnGuest.addEventListener('click', () => {
-    const urlGuest = document.getElementById('url_guest');
-    const urlGuestText = urlGuest.textContent.trim();
+    const url = document.getElementById('url_guest');
+    const urlText = url.textContent.trim();
     // ブラウザがClipboard APIに対応しているか判定
     if (navigator.clipboard != undefined) {
       // 対応している場合
-      navigator.clipboard.writeText(urlGuestText);
+      navigator.clipboard.writeText(urlText);
     } else {
       // 対応していない場合(IE)
-      window.clipboardData.setData('Text', urlGuestText);
+      window.clipboardData.setData('Text', urlText);
+    }
+  });
+  copyBtnHost.addEventListener('click', () => {
+    const url = document.getElementById('url_host');
+    const urlText = url.textContent.trim();
+    // ブラウザがClipboard APIに対応しているか判定
+    if (navigator.clipboard != undefined) {
+      // 対応している場合
+      navigator.clipboard.writeText(urlText);
+    } else {
+      // 対応していない場合(IE)
+      window.clipboardData.setData('Text', urlText);
     }
   });
 }
