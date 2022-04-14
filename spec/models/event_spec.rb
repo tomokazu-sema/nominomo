1185e7586e06169c51ff206e7e1fd30cf772bf68
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-14 20:40:54 +0900

    誤記修正

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe Event, type: :model do
 
   describe '日程調整ページ作成' do
     context '新規作成できる場合' do
-      it 'user、title、password_digestが存在する場合は登録できる' do
+      it 'user、title、password_digestが存在する場合は作成できる' do
         expect(@event).to be_valid
       end
-      it 'userが存在しなくても登録できる' do
+      it 'userが存在しなくても作成できる' do
         @event.user = nil
         expect(@event).to be_valid
       end
-      it 'password_digestが空でも登録できる' do
+      it 'password_digestが空でも作成できる' do
         @event.password_digest = ''
         expect(@event).to be_valid
       end
     end
 
     context '新規作成できない場合' do
-      it 'titleが空では登録できない' do
+      it 'titleが空では作成できない' do
         @event.title = ''
         @event.valid?
         expect(@event.errors.full_messages).to include("Title can't be blank")
