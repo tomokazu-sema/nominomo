5641b81f0a9e341e69b1a0e434548c32be8de50c
tomokazu-sema <t.sema.1986@gmail.com>
2022-04-18 13:37:06 +0900

    テストコード実行

--- spec/factories/event_places.rb
+++ spec/factories/event_places.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :event_place do
-    
+    place   { Faker::Restaurant.name }
+    url     { Faker::Internet.url }
+    comment { Faker::Restaurant.description }
+    association :event
   end
 end
