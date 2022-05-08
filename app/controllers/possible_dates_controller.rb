4128a3d6ba569bdff7619c370e5cb4018ad476c6
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-08 17:06:37 +0900

    候補日の編集機能実装

--- app/controllers/possible_dates_controller.rb
+++ app/controllers/possible_dates_controller.rb
@@ -1,20 +1,40 @@
 class PossibleDatesController < ApplicationController
+  before_action :set_event
+  before_action :set_possible_date, only: [:edit, :update]
+
   def new
-    @event = Event.find_by(uid: params[:event_id])
     @possible_date = PossibleDate.new
   end
 
   def create
-    @event = Event.find_by(uid: params[:event_id])
     @possible_date = PossibleDate.new(possible_date_params)
     if @possible_date.save
       respond_to do |format|
-        format.html { redirect_to @event }
+        format.html { redirect_to event_path(@event) }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @possible_date = @possible_date.update(possible_date_params)
+      respond_to do |format|
+        format.html { redirect_to event_path(@event) }
       end
     end
   end
 
   private
+
+  def set_event
+    @event = Event.find_by(uid: params[:event_id])
+  end
+
+  def set_possible_date
+    @possible_date = PossibleDate.find(params[:id])
+  end
 
   def possible_date_params
     params.require(:possible_date).permit(:datetime).merge(event_id: Event.find_by(uid: params[:event_id]).id)
