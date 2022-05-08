5b2008368b08549123cccba0f53ae04e68ea25ee
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-09 06:35:16 +0900

    active_hashの設定

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,7 @@
 class Attendance < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   # アソシエーション
+  belongs_to :answer
   belongs_to :possible_date
   belongs_to :guest
 end
