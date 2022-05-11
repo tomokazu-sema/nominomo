a69c650ae9a4a7eaee0ea8e063d13d9bb5bf0087
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-11 21:24:44 +0900

    お知らせ作成、編集機能を実装

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,2 +1,46 @@
 class NoticesController < ApplicationController
+  before_action :set_event
+  before_action :set_notice, only: %i[edit update]
+
+  def new
+    @notice = Notice.new
+  end
+
+  def create
+    @notice = Notice.new(notice_params)
+    respond_to do |format|
+      if @notice.save
+        format.html { redirect_to event_path(@event) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @notice.update(notice_params)
+        format.html { redirect_to event_path(@event) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find_by(uid: params[:event_id])
+  end
+
+  def set_notice
+    @notice = Notice.find(params[:id])
+  end
+
+  def notice_params
+    params.require(:notice).permit(:message).merge(event_id: @event.id)
+  end
 end
