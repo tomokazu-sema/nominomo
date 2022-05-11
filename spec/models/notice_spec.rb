d252f50680f4b97bc890a4cc4cf790ded057084c
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-11 21:31:29 +0900

    テストコードの実行

--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Notice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @notice = FactoryBot.build(:notice)
+  end
+
+  describe '日程調整ページお知らせ' do
+    context '新規作成できる場合' do
+      it 'event、messageが存在する場合は作成できる' do
+        expect(@notice).to be_valid
+      end
+      it 'messageが空でも作成できる' do
+        @notice.message = ''
+        expect(@notice).to be_valid
+      end
+    end
+
+    context '新規作成できない場合' do
+      it 'eventに紐付いていないと作成できない' do
+        @notice.event = nil
+        @notice.valid?
+        expect(@notice.errors.full_messages).to include('Event must exist')
+      end
+    end
+  end
 end
