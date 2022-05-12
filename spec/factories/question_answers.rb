b5ec122b6b24a34c1ad5dc34edbe8792b657919e
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-12 19:31:28 +0900

    テストコードの実行

--- spec/factories/question_answers.rb
+++ spec/factories/question_answers.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :question_answer do
-    
+    answer { Faker::Lorem.sentence }
+    association :guest
+    association :question
   end
 end
--- spec/models/question_answer_spec.rb
+++ spec/models/question_answer_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe QuestionAnswer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @question_answer = FactoryBot.build(:question_answer)
+  end
+
+  describe '日程調整ページアンケート回答' do
+    context '新規作成できる場合' do
+      it 'guest、question、answerが存在する場合は作成できる' do
+        expect(@question_answer).to be_valid
+      end
+      it 'answerが空でも作成できる' do
+        @question_answer.answer = ''
+        expect(@question_answer).to be_valid
+      end
+    end
+
+    context '新規作成できない場合' do
+      it 'guestに紐付いていないと作成できない' do
+        @question_answer.guest = nil
+        @question_answer.valid?
+        expect(@question_answer.errors.full_messages).to include('Guest must exist')
+      end
+      it 'questionに紐付いていないと作成できない' do
+        @question_answer.question = nil
+        @question_answer.valid?
+        expect(@question_answer.errors.full_messages).to include('Question must exist')
+      end
+    end
+  end
 end
