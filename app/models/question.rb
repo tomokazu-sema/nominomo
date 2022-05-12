dad19c97bcb3a7e9bbce6fb316f9f3346f546449
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-12 17:56:05 +0900

    アンケート回答機能実装

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,12 @@ class Question < ApplicationRecord
   # アソシエーション
   belongs_to :event
   has_many   :question_answers, dependent: :destroy
+
+  # アンケート用 引数guestのselfアンケートのquestion_answerモデルを返す
+  def question_answer(guest)
+    question_answers.each do |question_answer|
+      return question_answer if question_answer.guest_id == guest.id
+    end
+    nil
+  end
 end
