0775e19c515bf878a5f255155d87be75805e33cf
tomokazu-sema <t.sema.1986@gmail.com>
2022-05-12 21:26:00 +0900

    会費状況の入力、編集機能実装

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,55 @@
 class PaymentsController < ApplicationController
+  before_action :set_event
+  before_action :set_guest, only: %i[new edit]
+  before_action :set_payment_management, only: %i[new edit]
+  before_action :set_payment, only: %i[edit update]
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    respond_to do |format|
+      if @payment.save
+        format.html { redirect_to event_path(@event) }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @payment.update(payment_params)
+        format.html { redirect_to event_path(@event) }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.find_by(uid: params[:event_id])
+  end
+
+  def set_guest
+    @guest = Guest.find(params[:guest_id])
+  end
+
+  def set_payment_management
+    @payment_management = PaymentManagement.find(params[:payment_management_id])
+  end
+
+  def set_payment
+    @payment = Payment.find(params[:id])
+  end
+
+  def payment_params
+    params.require(:payment).permit(:guest_id, :price, :status_id).merge(payment_management_id: params[:payment_management_id])
+  end
 end
--- app/models/payment_management.rb
+++ app/models/payment_management.rb
@@ -2,4 +2,12 @@ class PaymentManagement < ApplicationRecord
   # アソシエーション
   belongs_to :event
   has_many   :payments, dependent: :destroy
+
+  # 会費管理用 引数guestに対応するpaymentモデルを返す
+  def payment(guest)
+    payments.each do |payment|
+      return payment if payment.guest_id == guest.id
+    end
+    nil
+  end
 end
